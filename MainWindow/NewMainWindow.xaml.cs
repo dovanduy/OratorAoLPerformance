a13fd44aa04f7f93f6785d2707e072cf35876646
xingmanzhao <xingman.zhao@gmail.com>
2014-05-27 21:18:35 +0800

    create NewMainWindow for updating new main UI

---
+++ MainWindow/NewMainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Orator.AoL.MainWindow
+{
+	/// <summary>
+	/// Interaction logic for NewMainWindow.xaml
+	/// </summary>
+	public partial class NewMainWindow : Window
+	{
+		public NewMainWindow()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
